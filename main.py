63ee7bbf05e4d6ca9fdfcb3d1d3f46bc7306ff81
brydie bartlett <brydiebartlett@icloud.com>
2025-07-23 16:36:53 +1000

    basic commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# welcome to the main file of the hedge project 
+# starting with the necessary imports for the assistant 
+# file name main.py - last edit 23/7/2025
+
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import pyjokes
+
